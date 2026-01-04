fa910840be67d6e2f166a4d73cb6c07f833cee68
Antonio Moroño <107795054+ar70n@users.noreply.github.com>
2026-01-04 22:47:10 +0000

    Add files via upload

---
+++ Algorithm Analysis/Quick and Merge Sort/sorting.h
@@ -0,0 +1,41 @@
+/**
+ *
+ * Descripcion: Header file for sorting functions 
+ *
+ * Fichero: sorting.h
+ * Autor: Carlos Aguirre
+ * Version: 1.0
+ * Fecha: 16-09-2019
+ *
+ */
+
+#ifndef SORTING_H
+#define SORTING_H
+
+/* constants */
+
+#ifndef ERR
+  #define ERR -1
+  #define OK (!(ERR))
+#endif
+
+/* type definitions */
+typedef int (* pfunc_sort)(int*, int, int);
+
+/* Functions */
+int partition(int *tabla, int ip, int iu, int *pos);
+int merge(int* tabla, int ip, int iu, int imedio);
+int SelectSort(int* array, int ip, int iu);
+int SelectSortInv(int* array, int ip, int iu);
+int min(int* array, int ip, int iu);
+int mergesort(int* tabla, int ip, int iu);
+int quicksort(int* tabla, int ip, int iu);
+int median_avg(int *tabla, int ip, int iu, int *pos);
+int median(int *tabla, int ip, int iu, int *pos);
+int swap(int *tabla, int i, int j);
+int median_stat(int *tabla, int ip, int iu, int *pos);
+
+
+
+
+#endif
